efd78e667e7e8658cae7cdcc52c7c1601146fd37
contt <contt@LUIZFERNANDO>
2023-04-18 20:26:32 -0300

    Adicionar arquivos de projeto.

---
+++ SegundoProjeto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Security.AccessControl;
+
+namespace hora
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int x = int.Parse(Console.ReadLine());
+
+            if ( x < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (x < 18) {
+                Console.WriteLine("Boa Tarde!!");
+            }
+            else
+            {
+                Console.WriteLine("Boa Noite!!");
+                
+            }
+
+        }
+    }
+
+}
+
